fc4950d63a73096a72ab93e87060220224808865
Nando Vieira <fnando.vieira@gmail.com>
2015-10-10 14:15:03 -0300

    Configure databases. Official support for MySQL and PostgreSQL.

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -1,8 +1,3 @@
-system 'rm spec/db/test.sqlite3 &> /dev/null'
-system 'mysqladmin -u root drop coupons_test --force &> /dev/null'
-system 'mysqladmin -u root create coupons_test --default-character-set=utf8'
-system 'dropdb coupons_test &> /dev/null; createdb coupons_test'
-
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../db/migrate', __FILE__)
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 ActiveRecord::Migrator.migrate(ActiveRecord::Migrator.migrations_paths)
